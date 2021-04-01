d44ebf4021098c39cb4e2220e292a34d4ae6a380
mmiirul <mmiirul00@gmail.com>
2021-04-01 13:32:59 +0800

    useEffect, useState, useRef commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import React, { useState } from 'react'
+import React, { useState , useEffect, useRef } from 'react'
 
 // function countInitial(){
 //   console.log("Render function Count");
@@ -10,6 +10,7 @@ import React, { useState } from 'react'
 
 export default function Home() {
 
+  /**useState Hook */
   /*const [count, setCount] = useState(countInitial()); //render on every run */
   const [state, setState] = useState({count: 4, theme: "blue"}); //render on first run
   const count = state.count;
@@ -28,12 +29,58 @@ export default function Home() {
       return {...prevState ,count: prevState.count +1 , theme: "red"}
     })
   }
+
+  /** useEffect Hook */
+  const [resourceType, setResourceType] = useState('posts');
+
+  useEffect(() => {
+    console.log("resource changed");
+
+    return () => {
+      console.log("return from resource changed"); //for cleanup
+    }
+  },[resourceType])
+
+  /** useRef Hook */
+  const [name, setName] = useState('');
+  const renderCount = useRef(1);
+  const prevName = useRef('');
+  const inputRef = useRef('')
+
+  useEffect(() => {
+    renderCount.current = renderCount.current +1
+    prevName.current = name
+  },[name])
+
+  function focus(){
+    inputRef.current.focus()
+    inputRef.current.value = 'amirul'
+  }
+
   return (
     <>
+    <h2>useState Hook</h2>
       <button onClick={decrementCount}>-</button>
       <span>{count}</span>
       <span>{theme}</span>
       <button onClick={incrementCount}>+</button>
+      <br />
+    <h2>useEffect Hook</h2>
+      <div>
+        <button onClick={() => setResourceType('posts')}>Posts</button>
+        <button onClick={() => setResourceType('users')}>Users</button>
+        <button onClick={() => setResourceType('comments')}>Comments</button>
+      </div>
+      <h3>{resourceType}</h3>
+      <br />
+      <h2>useRef Hook</h2>
+      <div>
+        <input ref={inputRef} value={name} onChange={e => setName(e.target.value)}/>
+        <div>My name is {name} and it used to be {prevName.current}</div>
+        <div>i render {renderCount.current} times</div>
+        <div>{inputRef.current.value}</div>
+        <button onClick={focus}>Focus</button>
+      </div>
     </>
   )
 }
